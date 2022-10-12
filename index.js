feaf0a8546b7a73b62ca8a7d29a16ac87bcc8732
Your Name <you@example.com>
2022-10-12 11:32:42 +0200

    Fájlok hozzáadása

---
+++ index.js
@@ -0,0 +1,18 @@
+let kaloria = 0;
+
+function visszaalit(){
+    console.log('Click');
+    document.getElementById('gomb').classList.toogle('Visszallit')
+}
+function init(){
+   
+    document.getElementById('gomb').addEventListener('click', click);
+    document.getElementById('paradicsom').addEventListener('click', paradicsom);
+}
+
+document.addEventListener('DOMContentLoaded',init);
+
+function paradicsom(){
+    documentum.getElementById('gomb').addEventListener('click', )
+
+}
